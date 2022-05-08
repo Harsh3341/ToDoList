4b0fb6b713ec6e8650d5fbea2d0995f5ca213a53
Harsh Gupta <harshgupta3341@gmail.com>
2022-05-08 20:52:36 +0530

    Added Date to ejs file

--- server.js
+++ server.js
@@ -8,11 +8,19 @@ app.set('view engine','ejs');
 app.get("/",function(req,res){
 
     var today = new Date();
-    var currentDay = today.getDay();
-    const dayName = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    // var currentDay = today.getDay();
+    // const dayName = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
-    res.render("index",{TodaysDay:dayName[currentDay]})
+    // res.render("index",{TodaysDay:dayName[currentDay]})
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
 
+    var day = today.toLocaleDateString("en-US",options);
+
+    res.render("index",{TodaysDay:day})
     
 });
 

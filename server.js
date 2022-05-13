bc6a9dd627fb3d709d41322498e848ce2d6d05cb
Harsh Gupta <harshgupta3341@gmail.com>
2022-05-13 13:17:10 +0530

    Updated all pages

--- server.js
+++ server.js
@@ -2,28 +2,49 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+var items = [];
+var workItems = [];
 
 
-app.get("/",function(req,res){
+app.get("/", function (req, res) {
 
     var today = new Date();
-    // var currentDay = today.getDay();
-    // const dayName = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
-    // res.render("index",{TodaysDay:dayName[currentDay]})
     var options = {
         weekday: "long",
         day: "numeric",
         month: "long"
     };
 
-    var day = today.toLocaleDateString("en-US",options);
+    var day = today.toLocaleDateString("en-US", options);
 
-    res.render("index",{TodaysDay:day})
-    
+    res.render("index", { TodaysDay: day, UpdatedItems: items });
+
 });
 
-app.listen(3000,function(){
+app.post("/", function (req, res) {
+    var item = req.body.newItem;
+
+    items.push(item);
+
+    res.redirect("/");
+});
+
+app.get("/work",function(req,res){
+    res.render('index',{TodaysDay: "Work List",UpdatedItems: workItems});
+});
+
+app.post("/work",function(req,res){
+    var item = req.body.newItem;
+
+    workItems.push(item);
+    res.redirect("/work");
+})
+
+app.listen(3000, function () {
     console.log("Server has started at port 3000");
 });  
